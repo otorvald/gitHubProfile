9860b2ef3984e08f4e7ce8a282ebc17f9822a4f3
Maksym Bystryk <Maksym.Bystryk@udelphi.com>
2018-07-31 12:20:23 +0300

    UI Implemented

--- GitHubProfileApp/GitHubProfileApp/EditProfileViewController.swift
+++ GitHubProfileApp/GitHubProfileApp/EditProfileViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class EditProfileViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
     
@@ -21,14 +23,24 @@ class EditProfileViewController: UIViewController {
     let textViewCellIdentifier = "textViewCell"
     
     let menuItems = ["Name", "Blog", "Company", "Location", "Bio"]
-    let textFieldPlaceholders = ["Name or Nickname", "example.com", "Company name", "City"];
+    let textFieldPlaceholders = ["Name or nickname", "Example.com", "Company name", "City"];
     
     let heightForTextFieldCell : CGFloat = 40;
-    let heightForTextViewCell : CGFloat = 120;
+    let heightForTextViewCell : CGFloat = 150;
+    
+    let bioMaxLength = 100;
+    let textFieldMaxLength = 25;
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.scrollView.bounces = false;
+        
+        self.profileImageViewBorder.layoutIfNeeded()
+        self.profileImageView.layoutIfNeeded()
+        self.profileImageViewBorder.layer.cornerRadius = self.profileImageViewBorder.frame.height/2;
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.height/2;
 
     }
     
@@ -42,12 +54,59 @@ class EditProfileViewController: UIViewController {
         
     }
     
+    func moveViewUp() {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.frame = CGRect(x:self.view.frame.origin.x, y:self.view.frame.origin.y - 200, width:self.view.frame.size.width, height:self.view.frame.size.height);
+        })
+    }
+    
+    func moveViewDown() {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.frame = CGRect(x:self.view.frame.origin.x, y:self.view.frame.origin.y + 200, width:self.view.frame.size.width, height:self.view.frame.size.height);
+        })
+    }
+    
 }
 
 extension EditProfileViewController: UITextViewDelegate {
-    func textViewDidChange(_ textView: UITextView) {
-        print(textView.text)
+    
+
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        self.moveViewUp()
     }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        self.moveViewDown()
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+}
+
+extension EditProfileViewController: UITextFieldDelegate {
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.moveViewUp()
+    }
+    
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.moveViewDown()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
 }
 
 extension EditProfileViewController: UITableViewDelegate, UITableViewDataSource {
@@ -55,6 +114,7 @@ extension EditProfileViewController: UITableViewDelegate, UITableViewDataSource 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.menuItems.count
     }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -66,9 +126,12 @@ extension EditProfileViewController: UITableViewDelegate, UITableViewDataSource 
         }
         
         let cell = tableView.dequeueReusableCell(withIdentifier: self.textFieldCellIdentifier, for: indexPath) as! TextFieldTableViewCell
-        cell.label.text = self.menuItems[indexPath.row]
+        cell.label.text = self.menuItems[indexPath.row];
+        cell.textField.placeholder = self.textFieldPlaceholders[indexPath.row];
+        cell.textField.delegate = self;
         return cell;
     }
+    
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
